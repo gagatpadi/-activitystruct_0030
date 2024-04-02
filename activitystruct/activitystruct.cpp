10b2356665edd1650fd994604a8121a7459b96f8
gagatpadi <padiyogaradhana@gmail.com>
2024-04-02 17:20:58 +0700

    int main

--- activitystruct/activitystruct.cpp
+++ activitystruct/activitystruct.cpp
@@ -13,11 +13,15 @@ int main() {
 	cout << "Nomor Mahasiswa : ";
 	cin >> mhs.nim;
 	cout << "Nama Mahasiswa : ";
-	cin >> mhs.nim;
+	cin >> mhs.nama;
 	cout << "Alamat Mahasiswa : ";
 	cin >> mhs.alamat;
 	cout << "Umur Mahasiswa : ";
 	cin >> mhs.umur;
 
-	
+	cout << endl;
+	cout << "\n NIM : " << mhs.nim;
+	cout << "\n Nama : " << mhs.nama;
+	cout << "\n Alamat : " << mhs.alamat;
+	cout << "\n Umur : " << mhs.umur;
 }
