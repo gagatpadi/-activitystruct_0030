5c4947414c0ac6b116bcc57bf8a4cb49274d9a1f
gagatpadi <padiyogaradhana@gmail.com>
2024-04-02 19:29:27 +0700

    for int

--- activitystruct/Source4.cpp
+++ activitystruct/Source4.cpp
@@ -24,4 +24,23 @@ int main()
         cout << "Nama Mahasiswa : ";
         getline(cin, mhs[i].nama);
 
-      
+        cout << "Alamat Mahasiswa : " << endl;
+        cout << "\t Nama Desa : ";
+        cin >> mhs[i].alamat.desa;
+        cout << "\t Nama Kota : ";
+        cin >> mhs[i].alamat.kota;
+
+        cout << "Umur Mahasiswa : ";
+        cin >> mhs[i].umur;
+        cin.ignore(1, '\n');
+    }
+    for (int i = 0; i < 3; i++) {
+        cout << endl;
+        cout << "\n NIM : " << mhs[i].nim;
+        cout << "\n Nama : " << mhs[i].nama;
+        cout << "\n Alamat : ";
+        cout << "\n \t Desa : " << mhs[i].alamat.desa;
+        cout << "\n \t Kota : " << mhs[i].alamat.kota;
+        cout << "\n Umur : " << mhs[i].umur;
+    }
+}
